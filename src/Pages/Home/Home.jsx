4da5aefd27f8be89b397cd35e3d995f29ee672ce
MuhDRifai <muh.d.rifai23@gmail.com>
2023-08-14 06:10:37 +0700

    products

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Slider from '../../Components/Slider/Slider'
 import "./Home.scss";
+import FeaturedProducts from '../../Components/FeatureProducts/FeaturedProducts';
 
 const Home = () => {
   return (
     <div className="home">
       <Slider/>
+      <FeaturedProducts type ="featured"/>
+      <FeaturedProducts type ="treanding"/>
     </div>
   )
 }
